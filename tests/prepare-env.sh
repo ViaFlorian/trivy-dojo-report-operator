3af8916c10b646b3a77abbc360fa4d9e2b4fda97
Florian Kretschmer <116546367+ViaFlorian@users.noreply.github.com>
2026-03-05 19:18:11 +0100

    refactor: reorder helm commands for improved clarity in environment preparation script

--- tests/prepare-env.sh
+++ tests/prepare-env.sh
@@ -8,5 +8,7 @@ if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 minikube start
+helm repo add aquasecurity https://aquasecurity.github.io/helm-charts/
+helm dependency build "${SCRIPT_DIR}/resources/helm/"
 helm install trivy-operator "${SCRIPT_DIR}/resources/helm/"
 kubectl create namespace defectdojo
